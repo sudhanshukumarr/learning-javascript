66d5a676de4d9f67619a45bba2df7e56917c6b8a
Sudhanshu Kumar <sudhanshusham57@gmail.com>
2022-09-04 22:05:47 +0530

    add s.js

---
+++ s.js
@@ -0,0 +1,581 @@
+// // const ageJonas = 2037-1991 ;
+// // console.log(ageJonas);
+ 
+
+
+
+
+// // const name = "jonas" ;
+// // const job = "teacher" ;
+// // const birthyear = 1998;
+// // const year = 2022;
+
+// // const jonas = "i'm " + name +  'a' + (year - birthyear) + 'years old' + job + '!' ;
+// // console.log (jonas);
+
+// // const jonasnew = ` i am ${name},a  ${year-birthyear} year old ${job}`;
+// // console.log(jonasnew);
+
+
+
+
+// // console.log(`helloworld' \n
+// // 'hey`);
+
+
+// // console.log(`hello
+// // baby
+// // how
+// // are
+// // you
+// // `);
+
+
+
+
+
+
+
+
+// // const age = 12 ;
+// // if(age>18){
+// //     console.log('you can drive car');
+// // }
+// // else{
+// //     const yearleft = 18 - age ;
+// //     console.log(`you are too young to drive car
+// //     you have to wait ${yearleft}  years  `);
+// // }
+
+
+
+
+// // const birthyear = 2012 ;
+
+// // let century ;
+// //  if (birthyear<2000){
+// //     century = 20;
+// //  }else{
+// //     century = 21;
+// //  }
+// //  console.log(century);
+
+
+// // function x(y, z){
+// //     let sum = 0;
+// //     for(i = 0; i < y.length; i++){
+// //         sum = y[ i] + sum;
+// //     }
+// //    return sum/y.length;
+// // }
+// // console.log(x( [1,2,7,3,9], 5));
+
+// // for (let i = 1; i<=5; i++){
+    
+// //         console.log('*'.repeat(i));
+// //     }
+
+
+// // const dolphinsscore = (96+108+89)/3;
+// // const koalasscore = (88+91+110)/3;
+
+// // console.log(dolphinsscore);
+// // console.log(koalasscore);
+
+// // if (dolphinsscore>koalasscore){
+// //     console.log( 'dolphin is the winner');
+// // } else{
+// //     console.log('koalas is the winner');
+// // }
+
+// // const inputyear = ('1991');
+// // console.log(inputyear+ 10);
+// // console.log(Number(inputyear));
+// // console.log(Number(inputyear)+10);
+// // console.log(Number('jonas'));
+// // console.log(typeof(NaN));
+// //  console.log( 'i am'   +   ' 23'     +  'years old'  );
+// //  console.log('10'+'5'+ '1');
+// //  console.log( 2+ 3+ '5');
+
+
+// // console.log(Boolean(0));
+// // console.log(Boolean(undefined));
+// // console.log(Boolean('sudhanshu'));
+// // console.log(Boolean({}));
+// // console.log(Boolean(1));
+
+
+// // const money= 1;
+// // if(money){
+// //     console.log('dont spend it all ');
+// // }else{
+// //     console.log('you should get a job');
+// // }
+
+
+
+
+
+
+// // const hasdriverliecesnce = true ;
+// // const hasgoodvision = true ;
+
+// // console.log(hasdriverliecesnce && hasgoodvision);
+// // console.log(hasdriverliecesnce || hasgoodvision);
+// // console.log(!hasdriverliecesnce);
+
+// // if(hasdriverliecesnce && hasgoodvision){
+// //     console.log('sarah should drive');
+// // }else{
+// // asdriverliecesnce && hasgoodvision && !istired){
+// //     console.log('sarah should drive');
+// // }else{
+// //     console.log('someone else should drive');
+// // }    console.log('someone else should drive');
+// // // }
+
+// // const istired = false ;
+
+// // if(h
+
+
+// // const day = 'saturday';
+
+// // switch(day){
+// //     case 'monday':
+//     //     console.log('plan course structure');
+//     //     console.log('go to coding meetup');
+//     //     break;
+    
+//     // case 'tuesday':
+//     //     console.log('prepare theory videos');
+//     //     break;
+        
+//     // case 'wednesday' :
+//     //     console.log('write code examples');
+// // //         break;
+// // //     case 'thursday' :
+// // //         console.log('record videos');
+// // //         break;
+// // //     case 'friday':
+// // //         console.log('record videos');
+// // //         break ;
+
+// // //     case 'saturday':
+// // //     case 'sunday':
+// // //         console.log('enjoy weekend');
+// // //         break;
+
+// // //       default:
+// // //         console.log('not a valid day');  
+// // // }
+
+// // const day = 'sunday';
+// // if(day === 'monday'){
+// //     console.log('plan course structure');
+// //     console.log('go to coding meetup');
+// // } else if ( day === 'tuesday') {
+// //         console.log('prepare theory videos');
+// // }else if(day=== 'wednesday'){
+// //     console.log('record videos');
+// // }else if(day==='thursday'){
+// //     console.log('write code examples');
+// // }else if(day==='friday'){
+// //     console.log('record videos');
+// // }else if(day==='saturday'){
+// //     console.log('enjoy the weekend');
+// // }else if(day==='sunday'){
+// //     console.log('enjoy the weekend');
+// // }else {
+// //     console.log('not a valid day');
+// // }
+
+
+// // const age = 19;
+// // const drink = age>=18? 'wine': 'water';
+// // // console.log(drink);
+// // console.log(`i like to drink ${age>=18 ? 'wine': 'water'}`);
+
+// // const bill = 275;
+// // const tip = bill<=300 && bill>=50 ? bill*0.15:bill*0.2;
+// // console.log(`the bill was${bill},the tip was${tip}, the total amount was${bill+tip}`);
+
+
+    
+//     // const else = 23;
+//     // console.log(else);
+    
+
+
+
+  
+// //console.log(arr);
+// // console.log(arr.length);
+// // console.log(arr[49]
+
+
+
+
+// // function even(a){
+// //      let arr = [];
+// //      for (let i = 1 ; i <= a ; i++){
+// //         if(i%2 == 0 )
+// //         {
+// //             arr.push(i)
+// //         }
+       
+// //      }
+// //      return arr;
+    
+    
+// // }
+// // console.log(even(10));
+
+// //  function x(y){
+// //     for(let i=0; i<=y.length ; i++){
+     
+// //     }
+// //     console.log(y[0]);
+// //     console.log(y[2]);
+// //     console.log(y[4]);
+// // }
+// //  console.log(x([22 ,23,19 ,55 ,10]));
+
+
+// // function x(y){
+// //     let arr = [];
+// //     for( let i=0; i<y.length ; i++){
+        
+// //     let z = (y[i]*(y[i+1]));
+// //     if (i<y.length){
+// //     console.log([z])}
+// //     }
+// //     return arr;
+// // }
+// // console.log(x([10,11,12,13]))
+
+
+
+// // const calcAge3 = birthyear => 2037- birthyear;
+// // const age3 = calcAge3(1991);
+// // console.log(age3); 
+
+// // function cutFruitPieces(fruit){
+// //     return fruit*4;
+// // }
+
+// // function fruitProcessor(apples, oranges){
+// //     const applePieces = cutFruitPieces(apples);
+// //     const orangePieces = cutFruitPieces(oranges);
+
+// //     const juice = `juice with ${applePieces}piece of apple and ${orangePieces} pieces of orange.`;
+// //     return juice ;
+
+// // }
+// // console.log(fruitProcessor(2,3));
+
+ 
+// // function fruitprocessor(apple, orange){
+// //    console.log(apple,orange);
+// //     const juice = `juice with ${apple}apples and ${orange}oranges`;
+// //     return juice;
+
+// // }
+
+// // fruitprocessor (1,2);
+// // //  const applejuice = fruitprocessor(1,2);
+// // console.log(fruitprocessor(1,2 )); 
+// // const appleorangejuice = fruitprocessor(2,5 );
+// // console.log(fruitprocessor(2,5)); 
+
+
+// // function calcage1(birthyear){
+// //     return 2037 - birthyear;
+// // }
+// // //console.log(calcage1(1991)); 
+
+
+
+// // const calcage2 = function(birthyear){
+// //     return 2037 - birthyear ;
+
+// // }
+// // console.log(calcage1(1991)),console.log(calcage2(1991));
+
+
+
+// // function cutfruitpieces(fruit){
+// //     return  fruit * 4;
+// // }
+
+// // function fruitprocessor(apple, orange){
+// //     const applepieces = cutfruitpieces(apple);
+// //     const orangepieces = cutfruitpieces(orange);
+   
+// //     const juice = `juice with ${applepieces}apples and ${orangepieces}oranges`;
+// //     return juice;
+// // }
+
+// // console.log(fruitprocessor(1,2))
+
+
+// // function calcage(birthyear){
+// //     return 2037- birthyear ;
+// // }
+
+// // function  abc(birthyear, name){
+// //     const age = calcage(birthyear);
+// //     const retirement = 65-age;
+// //     if (retirement > 0){
+// //         return retirement ;
+// //     }else{
+// //         return -1;
+// //     }
+    
+// // }
+// // console.log(abc(1991,'jonas'));
+// // console.log(abc(1970,'john'));
+
+
+// // function x(a,b,c,d){
+    
+// //     if (!d){
+// //         d =10
+// //     }
+    
+    
+// //     let z = a+d;
+// //     let y = b*c;
+// //     const avg = (z+y)/2;
+// //     return avg;
+// // }
+// // console.log(x(1,2,3));
+
+
+
+
+// // x = []
+// // if(x.length = 0){
+// // console.log('true');
+// // }else{
+// //     console.log('false');
+// // }
+
+
+// // function x(y){
+//   //   console.log(typeof y);
+    
+// //     if (typeof y == 'number'){
+// //         console.log('you entered a number')
+// //     }else if (typeof y=='string'){
+// //         console.log('you entered a string')
+// //     }else if (typeof y == 'object'){
+// //         console.log('you entered an array');
+// //    }
+// //     console.log(typeof y);
+//  //} 
+// // console.log(x(undefined));
+
+// // cnds[0]);
+// // console.log(friends[2]);
+// //  console.log(friends[friends.length - 1]);
+// //  friends[2] = 'jay';
+// //  console.log(friends);
+
+// //  const firstname = 'jonas';
+// //  const jonas = [firstname,'schmetdman',2037-1991,'teacher' , friends];
+// //  console.log(jonas);
+// // console.log(friends.length);
+// // console.log(jonas.length);
+// // onsole.log(frie
+
+
+// // function calcage(birthyear){
+// //  const age = 2037- birthyear;
+// // return age ; 
+  
+// // }
+// // const years = [1990,1992] ;
+// // const age1 = calcage(years[0]);
+// // const age2 = calcage(years[1]);
+// // console.log(age1,age2);
+// //  const ages = [calcage(years[0]), calcage(years[1])];
+// //  console.log(ages);
+
+
+// const friends = ['Micheal', 'steven','peter'];
+// friends.push('jay');
+// console.log(friends);
+
+// friends.unshift('john');
+// console.log(friends);
+// friends.pop();
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends);
+
+// friends.shift ();
+// console.log(friends);
+
+
+// console.log(friends.indexOf('steven'));
+
+
+
+
+
+// if (friends.includes('steven')){
+//   console.log('you have a friend called steven');
+// }
+
+
+// function x(y,z){
+  
+//   if(z.includes(y)){
+//     return true;
+//   }else{
+//     return false;
+//   }
+ 
+// }
+// console.log(x(1,[10,2,3]));
+
+
+
+
+
+
+// function x(a,b,c){
+//  let d = [];
+//   for(let i = 0; i<a.length; i++){
+//    d.push((a[i] * b) + c );  
+//   }
+//   return d;
+// }
+// console.log(x([1,2,3],2,3));
+
+// function x(y,k){
+//   let d = [];
+//   for (let i = 0; i<y.length; i++){
+//     if (y[i] % 2 == 0){
+//       if(d.length < k){
+//         d.push(y[i]); 
+
+//       }else{
+//         return d ;
+//       }
+          
+//     }    
+//   }
+//   return d;
+// }
+// console.log(x([1, 3, 7, 2,3, 4,6,8,9,20], 4));
+
+
+// function x (a,b,c){
+//   let d = [];
+//   for(i = 0; i<a.length; i++){
+//     if((a[i]/b)>c){
+//        d.push(a[i]);
+//     }
+//   }
+//   return d;
+// }
+
+// console.log(x([1,5,10],2,3));
+
+
+
+
+// function x(y){
+//   let d = [];
+//   for(let i = 0 ; i<y.length; i++){
+//     if(i %2 == 0){
+//       d.push(y[i]);
+
+//     }
+//   }
+//   return d;
+// }
+// console.log(x([1,2,3,4,5]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
